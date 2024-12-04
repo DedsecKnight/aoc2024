321a2875e6c45f2dfbaf8374cd988ce76323ad74
DedsecKnight <mikenguyen7268@gmail.com>
2024-12-03 23:14:20 -0600

    added day 4 solution

--- src/day4/part01.cpp
+++ src/day4/part01.cpp
@@ -1,3 +1,29 @@
 #include "aoc2024/sol.hpp"
 
-SOLUTION {}
+constexpr const array<int, 8> dx{-1, 1, 0, 0, 1, 1, -1, -1};
+constexpr const array<int, 8> dy{0, 0, -1, 1, 1, -1, 1, -1};
+
+SOLUTION {
+  vector<string> grid;
+  string line;
+  while (cin >> line) {
+    grid.push_back(line);
+  }
+  size_t n = grid.size(), m = grid[0].size();
+  int ret = 0;
+  for (size_t i = 0; i < n; i++) {
+    for (size_t j = 0; j < m; j++) {
+      if (grid[i][j] == 'X') {
+        for (size_t k = 0; k < 8; k++) {
+          if (i + dx[k] * 3 >= 0 && i + dx[k] * 3 < n && j + dy[k] * 3 >= 0 &&
+              j + dy[k] * 3 < m) {
+            ret += grid[i + dx[k]][j + dy[k]] == 'M' &&
+                   grid[i + dx[k] * 2][j + dy[k] * 2] == 'A' &&
+                   grid[i + dx[k] * 3][j + dy[k] * 3] == 'S';
+          }
+        }
+      }
+    }
+  }
+  cout << ret << '\n';
+}
--- src/day4/part02.cpp
+++ src/day4/part02.cpp
@@ -1,3 +1,28 @@
 #include "aoc2024/sol.hpp"
 
-SOLUTION {}
+SOLUTION {
+  vector<string> grid;
+  string line;
+  while (cin >> line) {
+    grid.push_back(line);
+  }
+  size_t n = grid.size(), m = grid[0].size();
+  int ret = 0;
+  for (size_t i = 1; i + 1 < n; i++) {
+    for (size_t j = 1; j + 1 < m; j++) {
+      if (grid[i][j] == 'A') {
+        string t1{}, t2{};
+        t1 += grid[i - 1][j - 1];
+        t1 += grid[i][j];
+        t1 += grid[i + 1][j + 1];
+
+        t2 += grid[i - 1][j + 1];
+        t2 += grid[i][j];
+        t2 += grid[i + 1][j - 1];
+
+        ret += (t1 == "MAS" || t1 == "SAM") && (t2 == "MAS" || t2 == "SAM");
+      }
+    }
+  }
+  cout << ret << '\n';
+}
