52e52d99aab6e497c4634feb47d1aa7aaec76467
DedsecKnight <mikenguyen7268@gmail.com>
2024-12-09 00:07:37 -0600

    added day 9 solution

--- src/day9/part01.cpp
+++ src/day9/part01.cpp
@@ -1,3 +1,37 @@
 #include "aoc2024/sol.hpp"
 
-SOLUTION {}
+SOLUTION {
+  string s;
+  cin >> s;
+  vector<string> mem;
+  int empty_slot = 0;
+  for (int i = 0, c = 0; i < int(s.size()); i += 2, c++) {
+    for (int j = 0; j < s[i] - '0'; j++) {
+      mem.push_back(to_string(c));
+    }
+    for (int j = 0; j < s[i + 1] - '0'; j++) {
+      mem.push_back(".");
+      empty_slot++;
+    }
+  }
+  int ptr1 = 0, ptr2 = int(mem.size()) - 1;
+  for (int iter = 0; iter < empty_slot; iter++) {
+    while (ptr1 < int(mem.size()) && mem[ptr1] != ".") {
+      ptr1++;
+    }
+    while (ptr2 >= 0 && mem[ptr2] == ".") {
+      ptr2--;
+    }
+    if (ptr1 > ptr2)
+      break;
+    assert(ptr1 != ptr2);
+    swap(mem[ptr1], mem[ptr2]);
+  }
+  ll ret = 0;
+  for (size_t i = 0; i < mem.size(); i++) {
+    if (mem[i] == ".")
+      break;
+    ret += i * 1ll * stoll(mem[i]);
+  }
+  cout << ret << '\n';
+}
--- src/day9/part02.cpp
+++ src/day9/part02.cpp
@@ -1,3 +1,56 @@
 #include "aoc2024/sol.hpp"
 
-SOLUTION {}
+struct file_t {
+  int s, e, id;
+};
+
+struct block_t {
+  int s, e, l;
+};
+
+SOLUTION {
+  string s;
+  cin >> s;
+  vector<file_t> files, final_files;
+  vector<block_t> blocks;
+  int pos = 0;
+  for (int i = 0, c = 0; i < int(s.size()); i += 2, c++) {
+    files.push_back({.s = pos, .e = pos + int(s[i] - '0') - 1, .id = c});
+    pos += s[i] - '0';
+    blocks.push_back(
+        {.s = pos, .e = pos + int(s[i + 1] - '0') - 1, .l = s[i + 1] - '0'});
+    pos += s[i + 1] - '0';
+  }
+  for (int i = int(files.size()) - 1; i >= 0; i--) {
+    for (int j = 0; j < int(blocks.size()); j++) {
+      if (blocks[j].s > files[i].s) {
+        final_files.push_back(files[i]);
+        break;
+      }
+      if (blocks[j].l >= files[i].e - files[i].s + 1) {
+        int sz = files[i].e - files[i].s + 1;
+        blocks.push_back({.s = files[i].s,
+                          .e = files[i].e,
+                          .l = files[i].e - files[i].s + 1});
+        if (blocks[j].s + sz <= blocks[j].e) {
+          blocks.push_back({.s = blocks[j].s + sz,
+                            .e = blocks[j].e,
+                            .l = blocks[j].e - blocks[j].s - sz + 1});
+        }
+        final_files.push_back(
+            {.s = blocks[j].s, .e = blocks[j].s + sz - 1, .id = files[i].id});
+        blocks.erase(blocks.begin() + j);
+        sort(blocks.begin(), blocks.end(),
+             [&](const auto &x, const auto &y) { return x.s < y.s; });
+        break;
+      }
+    }
+  }
+  ll ret = 0;
+  for (auto elem : final_files) {
+    for (int j = elem.s; j <= elem.e; j++) {
+      ret += j * 1ll * elem.id;
+    }
+  }
+  cout << ret << '\n';
+}
