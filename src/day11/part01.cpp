5eacce609d00eb28ef0f68c7e3b9e49d23c995cd
DedsecKnight <mikenguyen7268@gmail.com>
2024-12-10 23:28:16 -0600

    added day 11 solution

--- src/day11/part01.cpp
+++ src/day11/part01.cpp
@@ -1,3 +1,27 @@
 #include "aoc2024/sol.hpp"
 
-SOLUTION {}
+SOLUTION {
+  vector<string> nums;
+  string elem;
+  while (cin >> elem) {
+    nums.push_back(elem);
+  }
+  auto upd = [](const string &s) -> vector<string> {
+    if (s == "0")
+      return {"1"};
+    if (s.size() % 2 == 0) {
+      return {to_string(stoll(s.substr(0, s.size() / 2))),
+              to_string(stoll(s.substr(s.size() / 2, s.size() / 2)))};
+    }
+    return {to_string(stoll(s) * 2024)};
+  };
+  for (int iter = 0; iter < 25; iter++) {
+    vector<string> nxt;
+    for (const auto &elem : nums) {
+      auto tmp = upd(elem);
+      nxt.insert(nxt.end(), tmp.begin(), tmp.end());
+    }
+    nums.swap(nxt);
+  }
+  cout << nums.size() << '\n';
+}
--- src/day11/part02.cpp
+++ src/day11/part02.cpp
@@ -1,3 +1,30 @@
 #include "aoc2024/sol.hpp"
 
-SOLUTION {}
+SOLUTION {
+  vector<string> nums;
+  string elem;
+  while (cin >> elem) {
+    nums.push_back(elem);
+  }
+  map<pair<string, int>, ll> memo;
+  function<ll(string, int)> solve = [&](string s, int step) {
+    auto k = make_pair(s, step);
+    if (memo.find(k) != memo.end())
+      return memo[k];
+    if (step == 0)
+      return memo[k] = 1;
+    if (s == "0") {
+      return memo[k] = solve("1", step - 1);
+    }
+    if (s.size() % 2 == 0) {
+      auto left = to_string(stoll(s.substr(0, s.size() / 2)));
+      auto right = to_string(stoll(s.substr(s.size() / 2, s.size() / 2)));
+      return memo[k] = solve(left, step - 1) + solve(right, step - 1);
+    }
+    return memo[k] = solve(to_string(stoll(s) * 2024), step - 1);
+  };
+  ll ret =
+      accumulate(nums.begin(), nums.end(), 0ll,
+                 [&](auto acc, const auto &s) { return acc + solve(s, 75); });
+  cout << ret << '\n';
+}
